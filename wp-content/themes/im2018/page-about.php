1e1b260a0cf4dafe36d0368049452cd7878dcda4
Christina Deemer <cahdeemer@gmail.com>
2017-12-15 16:27:22 -0500

    added im2018 theme based on underscores

---
+++ wp-content/themes/im2018/page-about.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: About
+ *
+ * @package im2018
+ * @since im2018 1.0
+ */
+get_header(); ?>
+
+<main>
+	<h1>About Template</h1>
+	
+	
+</main>
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/im2018/page-careers.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Careers
+ *
+ * @package im2018
+ * @since im2018 1.0
+ */
+get_header(); ?>
+
+<main>
+	<h1>Careers Template</h1>
+	
+	
+</main>
+
+<?php get_footer(); ?>
