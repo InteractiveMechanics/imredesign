149d0ada53e934d9c51668f6de2611da801a847c
Christina Deemer <cahdeemer@gmail.com>
2018-01-19 09:52:58 -0500

    about page, some other things

--- wp-content/themes/im2018/page-about.php
+++ wp-content/themes/im2018/page-about.php
@@ -8,7 +8,40 @@
 get_header(); ?>
 
 <main>
-	<h1>About Template</h1>
+	<div class="jumbotron">
+			<div class="media-container">
+				<img src="<?php printThemePath(); ?>/assets/backgrounds/background-01@1x.jpg" />
+			</div>
+			
+			<div class="container-fluid">
+				<div class="row">
+					
+					<div class="col-sm-7">
+						<h1>Beautiful digital experiences for museums and archives</h1>
+					</div>
+					<div class="col-sm-7">
+						<p>We work on projects that empower and educate, and always bring honesty and authenticity to our projects.</p>
+						
+					</div>
+					
+				</div>
+  			</div>
+	</div>
+	
+	<article id="about-main">
+		<section class="container-fluid">
+			<div class="row">
+				<div class="col-sm-8 col-sm-offset-2 content-wrapper">
+					<p class="lead">As part of the re-installation of "Drawing Room from a Town House: 901 Fifth Aveue, New York City," the Philadelphia Museum of Art reached out to us to create  a pair of high-resolution digital interactives to allow visitors to take a closer look at 66 of the room's objects.</p>
+					
+					
+				
+				</div>
+			</div>
+		
+		</section>
+	</article>
+
 	
 	
 </main>
