98aaab78e859843917113c8feb10a513eb01f9e2
Christina Deemer <cahdeemer@gmail.com>
2018-01-19 17:25:14 -0500

    about page stuff

--- wp-content/themes/im2018/page-about.php
+++ wp-content/themes/im2018/page-about.php
@@ -34,6 +34,114 @@ get_header(); ?>
 				<div class="col-sm-8 col-sm-offset-2 content-wrapper">
 					<p class="lead">As part of the re-installation of "Drawing Room from a Town House: 901 Fifth Aveue, New York City," the Philadelphia Museum of Art reached out to us to create  a pair of high-resolution digital interactives to allow visitors to take a closer look at 66 of the room's objects.</p>
 					
+					<h3>Our Values</h3>
+					
+					<div class="values-wrapper">
+					
+						<div class="col-sm-2">
+						
+							<img src="<?php printThemePath(); ?>/assets/value-icons/value-icon-01@1x.png" />
+						
+						</div>
+						
+						<div class="col-sm-10">
+							
+							<h5>Communication / Openness / Empathy</h5>
+							<p>We've worked with clients from around the corner to across the country, and we've learned that open, ongoing communication is key. We've been commended for our ability to identify relevant stakeholders, to listen, to facilitate collaboration, and to explain our recommendations.</p>
+						
+						</div>
+				
+					</div>
+					
+					<div class="values-wrapper">
+					
+						<div class="col-sm-2">
+						
+							<img src="<?php printThemePath(); ?>/assets/value-icons/value-icon-01@1x.png" />
+						
+						</div>
+						
+						<div class="col-sm-10">
+							
+							<h5>Communication / Openness / Empathy</h5>
+							<p>We've worked with clients from around the corner to across the country, and we've learned that open, ongoing communication is key. We've been commended for our ability to identify relevant stakeholders, to listen, to facilitate collaboration, and to explain our recommendations.</p>
+						
+						</div>
+				
+					</div>
+					
+					<div class="values-wrapper">
+					
+						<div class="col-sm-2">
+						
+							<img src="<?php printThemePath(); ?>/assets/value-icons/value-icon-01@1x.png" />
+						
+						</div>
+						
+						<div class="col-sm-10">
+							
+							<h5>Communication / Openness / Empathy</h5>
+							<p>We've worked with clients from around the corner to across the country, and we've learned that open, ongoing communication is key. We've been commended for our ability to identify relevant stakeholders, to listen, to facilitate collaboration, and to explain our recommendations.</p>
+						
+						</div>
+				
+					</div>
+					
+					<div class="values-wrapper">
+					
+						<div class="col-sm-2">
+						
+							<img src="<?php printThemePath(); ?>/assets/value-icons/value-icon-01@1x.png" />
+						
+						</div>
+						
+						<div class="col-sm-10">
+							
+							<h5>Communication / Openness / Empathy</h5>
+							<p>We've worked with clients from around the corner to across the country, and we've learned that open, ongoing communication is key. We've been commended for our ability to identify relevant stakeholders, to listen, to facilitate collaboration, and to explain our recommendations.</p>
+						
+						</div>
+				
+					</div>
+					
+					<div class="values-wrapper">
+					
+						<div class="col-sm-2">
+						
+							<img src="<?php printThemePath(); ?>/assets/value-icons/value-icon-01@1x.png" />
+						
+						</div>
+						
+						<div class="col-sm-10">
+							
+							<h5>Communication / Openness / Empathy</h5>
+							<p>We've worked with clients from around the corner to across the country, and we've learned that open, ongoing communication is key. We've been commended for our ability to identify relevant stakeholders, to listen, to facilitate collaboration, and to explain our recommendations.</p>
+						
+						</div>
+				
+					</div>
+					
+					
+					<div class="values-wrapper">
+					
+						<div class="col-sm-2">
+						
+							<img src="<?php printThemePath(); ?>/assets/value-icons/value-icon-01@1x.png" />
+						
+						</div>
+						
+						<div class="col-sm-10">
+							
+							<h5>Communication / Openness / Empathy</h5>
+							<p>We've worked with clients from around the corner to across the country, and we've learned that open, ongoing communication is key. We've been commended for our ability to identify relevant stakeholders, to listen, to facilitate collaboration, and to explain our recommendations.</p>
+						
+						</div>
+				
+					</div>
+					
+					<div class="col-sm-12 center">
+					<a class="btn btn-default" href="#" role="button">Explore Our Work</a>
+					</div>
 					
 				
 				</div>
@@ -42,6 +150,240 @@ get_header(); ?>
 		</section>
 	</article>
 
+		
+	<article class="testimonial" style="background: url('<?php printThemePath(); ?>/assets/testimonial-backgrounds/testimonial-background-01@1x.jpg');">
+		<section class="container-fluid">
+			<div class="row">
+				<div class="col-sm-8 col-sm-offset-2 content-wrapper">
+					<blockquote class="blockquote">
+						<p>I've worked with dozens of developers and Interactive Mechanics was as friendly, reliable, and on-point as they come. They hit every project deadline and delivered an end product that exceeded our expectations.</p>
+						<footer class="blockquote-footer">
+							<h5 class="blockquote-name">Owen Henkel</h5>
+							<h5 class="blockquote-title">Director of Fund Development</h5>
+							<h5 class="blockquote-affiliation">Social Venture Fund</h5>  
+						</footer>
+					</blockquote>
+				
+				</div>
+			</div>
+		</section>
+	</article>
+	
+	
+	<article id="related-content" class="team">
+		<section class="container-fluid">
+			<div class="row">
+				
+					<div class="col-sm-12 related-content-heading">
+						<h3>Our Team</h3>
+					</div>
+				
+			
+					<div class="col-sm-4">
+						
+						<a href="#" class="feat-content-block" style="background: linear-gradient(45deg, rgba(71,142,187, 0.7), rgba(58,89,141, 0.7)), url('<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg');">
+							
+							<h5>Director of Strategic Initiatives</h5>
+							
+							<h3>Amelia Longo</h3>
+							
+						</a>
+					</div>
+					
+					<div class="col-sm-4">
+						
+						<a href="#" class="feat-content-block" style="background: linear-gradient(45deg, rgba(71,142,187, 0.7), rgba(58,89,141, 0.7)), url('<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg');">
+							
+							<h5>Chief Communications Officer</h5>
+							
+							<h3>Nyota Uhura</h3>
+							
+						</a>
+					</div>
+					
+					<div class="col-sm-4">
+						
+						<a href="#" class="feat-content-block" style="background: linear-gradient(45deg, rgba(71,142,187, 0.7), rgba(58,89,141, 0.7)), url('<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg');">
+							
+							<h5>Interactive Designer</h5>
+							
+							<h3>Tara Espenshade</h3>
+							
+						</a>
+					</div>
+					
+					<div class="col-sm-4">
+						
+						<a href="#" class="feat-content-block" style="background: linear-gradient(45deg, rgba(71,142,187, 0.7), rgba(58,89,141, 0.7)), url('<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg');">
+							
+							<h5>Marketing Manager</h5>
+							
+							<h3>LaNeshe Miller-White</h3>
+							
+						</a>
+					</div>
+					
+					<div class="col-sm-4">
+						
+						<a href="#" class="feat-content-block" style="background: linear-gradient(45deg, rgba(71,142,187, 0.7), rgba(58,89,141, 0.7)), url('<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg');">
+							
+							<h5>Defense Against The Dark Arts</h5>
+							
+							<h3>Severus Snape</h3>
+							
+						</a>
+					</div>		
+									
+			</div>
+		</section>
+	</article>
+	
+	
+	<article id="client-blocks-container">
+			<section class="container-fluid">
+				<div class="row">
+					
+					<div class="col-sm-12 related-content-heading">
+						<h3>Our Clients</h3>
+					</div>
+					
+						
+					<div class="col-sm-3">
+						<div class="client-block">
+							<a href="#"><img src="<?php printThemePath(); ?>/assets/clients-partners-logos/client-logo-philamuseum.png" /></a>
+						</div>
+					</div>
+					
+					<div class="col-sm-3">
+						<div class="client-block">
+							<a href="#"><img src="<?php printThemePath(); ?>/assets/clients-partners-logos/client-logo-philamuseum.png" /></a>
+						</div>
+					</div>
+					
+					<div class="col-sm-3">
+						<div class="client-block">
+							<a href="#"><img src="<?php printThemePath(); ?>/assets/clients-partners-logos/client-logo-philamuseum.png" /></a>
+						</div>
+					</div>
+					
+					<div class="col-sm-3">
+						<div class="client-block">
+							<a href="#"><img src="<?php printThemePath(); ?>/assets/clients-partners-logos/client-logo-philamuseum.png" /></a>
+						</div>
+					</div>
+					
+					<div class="col-sm-3">
+						<div class="client-block">
+							<a href="#"><img src="<?php printThemePath(); ?>/assets/clients-partners-logos/client-logo-philamuseum.png" /></a>
+						</div>
+					</div>
+					
+					<div class="col-sm-3">
+						<div class="client-block">
+							<a href="#"><img src="<?php printThemePath(); ?>/assets/clients-partners-logos/client-logo-philamuseum.png" /></a>
+						</div>
+					</div>
+					
+					<div class="col-sm-3">
+						<div class="client-block">
+							<a href="#"><img src="<?php printThemePath(); ?>/assets/clients-partners-logos/client-logo-philamuseum.png" /></a>
+						</div>
+					</div>
+					
+					<div class="col-sm-3">
+						<div class="client-block">
+							<a href="#"><img src="<?php printThemePath(); ?>/assets/clients-partners-logos/client-logo-philamuseum.png" /></a>
+						</div>
+					</div>	
+					
+					<div class="col-sm-3">
+						<div class="client-block">
+							<a href="#"><img src="<?php printThemePath(); ?>/assets/clients-partners-logos/client-logo-philamuseum.png" /></a>
+						</div>
+					</div>
+					
+					<div class="col-sm-3">
+						<div class="client-block">
+							<a href="#"><img src="<?php printThemePath(); ?>/assets/clients-partners-logos/client-logo-philamuseum.png" /></a>
+						</div>
+					</div>														
+						
+				</div>
+			</section>
+	</article>
+	
+	<article class="testimonial" style="background: url('<?php printThemePath(); ?>/assets/testimonial-backgrounds/testimonial-background-01@1x.jpg');">
+		<section class="container-fluid">
+			<div class="row">
+				<div class="col-sm-8 col-sm-offset-2 content-wrapper">
+					<blockquote class="blockquote">
+						<p>I've worked with dozens of developers and Interactive Mechanics was as friendly, reliable, and on-point as they come. They hit every project deadline and delivered an end product that exceeded our expectations.</p>
+						<footer class="blockquote-footer">
+							<h5 class="blockquote-name">Owen Henkel</h5>
+							<h5 class="blockquote-title">Director of Fund Development</h5>
+							<h5 class="blockquote-affiliation">Social Venture Fund</h5>  
+						</footer>
+					</blockquote>
+				
+				</div>
+			</div>
+		</section>
+	</article>
+	
+	<article id="partners-blocks-container">
+			<section class="container-fluid">
+				<div class="row">
+					
+					<div class="col-sm-12 related-content-heading">
+						<h3>Our Partners</h3>
+					</div>
+					
+						
+					<div class="col-sm-3">
+						<div class="client-block">
+							<a href="#"><img src="<?php printThemePath(); ?>/assets/clients-partners-logos/partner-logo-metcalfe.png" /></a>
+						</div>
+					</div>
+					
+					<div class="col-sm-3">
+						<div class="client-block">
+							<a href="#"><img src="<?php printThemePath(); ?>/assets/clients-partners-logos/partner-logo-metcalfe.png" /></a>
+						</div>
+					</div>
+					
+					<div class="col-sm-3">
+						<div class="client-block">
+							<a href="#"><img src="<?php printThemePath(); ?>/assets/clients-partners-logos/partner-logo-metcalfe.png" /></a>
+						</div>
+					</div>
+					
+					<div class="col-sm-3">
+						<div class="client-block">
+							<a href="#"><img src="<?php printThemePath(); ?>/assets/clients-partners-logos/partner-logo-metcalfe.png" /></a>
+						</div>
+					</div>
+					
+					<div class="col-sm-3">
+						<div class="client-block">
+							<a href="#"><img src="<?php printThemePath(); ?>/assets/clients-partners-logos/partner-logo-metcalfe.png" /></a>
+						</div>
+					</div>
+					
+					<div class="col-sm-3">
+						<div class="client-block">
+							<a href="#"><img src="<?php printThemePath(); ?>/assets/clients-partners-logos/partner-logo-metcalfe.png" /></a>
+						</div>
+					</div>											
+						
+				</div>
+			</section>
+	</article>
+
+
+
+
+	
+	
 	
 	
 </main>
