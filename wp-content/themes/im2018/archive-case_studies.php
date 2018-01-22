034775ae0bb1242c579bc55422d28c8781fe1bc0
Christina Deemer <cahdeemer@gmail.com>
2018-01-22 13:47:39 -0500

    case studies archive page

---
+++ wp-content/themes/im2018/archive-case_studies.php
@@ -0,0 +1,192 @@
+<?php
+/**
+ * Template Name: Case Studies Archive
+ *
+ * @package im2018
+ * @since im2018 1.0
+ */
+get_header(); ?>
+
+<main>
+	<div class="jumbotron jumbo-short">
+			<div class="media-container">
+				<img src="<?php printThemePath(); ?>/assets/backgrounds/background-01@1x.jpg" />
+			</div>
+			
+			<div class="container-fluid">
+				<div class="row">
+					
+					<div class="col-sm-7">
+						<h1>Our Work</h1>
+					</div>
+					
+					<div class="col-sm-7">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent maximus mi et orci efficitur pulvinar.</p>
+			
+					</div>
+					
+				</div>	
+  			</div>
+	</div>
+	
+	<article id="cs-archive">
+		<section class="container-fluid">
+				<div class="row">
+					<div class="col-sm-12">
+						<ul class="filter-group">
+							<li><a href="#">All Work</a></li>
+							<li><a href="#">Interactive Exhibits</a></li>
+							<li><a href="#">Websites</a></li>			
+						</ul>
+					</div>
+					
+					
+					<div class="col-sm-12">
+						<a class="feat-content-block-wide"  style="background: linear-gradient(rgba(45,49,66, 0.75), rgba(45,49,66, 0.75)), url('<?php printThemePath(); ?>/assets/featured-images/featured-image-01@1x.jpg');">
+							
+							<h5>Interactive Exhibits</h5>
+							
+							<h3>Muscle Hatchery Exhibits</h3>
+							
+							<img src="<?php printThemePath(); ?>/assets/clients-partners-logos/client-logo-white-philamuseum.png" />
+							
+														
+						</a>
+					</div>
+					
+								
+					<div class="col-sm-4">
+						
+						<a href="#" class="feat-content-block" style="background: linear-gradient(rgba(45,49,66, 0.75), rgba(45,49,66, 0.75)), url('<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg');">
+							
+							<h5>Interactive Exhibits</h5>
+							
+							<h3>Zhizua Temple Ceiling Interactive</h3>
+							
+							<img src="<?php printThemePath(); ?>/assets/clients-partners-logos/client-logo-white-philamuseum.png" />
+
+							
+						</a>
+					</div>
+					
+					<div class="col-sm-4">
+						
+						<a href="#" class="feat-content-block" style="background: linear-gradient(rgba(45,49,66, 0.75), rgba(45,49,66, 0.75)), url('<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg');">
+							
+							<h5>Interactive Exhibits</h5>
+							
+							<h3>Zhizua Temple Ceiling Interactive</h3>
+							
+							<img src="<?php printThemePath(); ?>/assets/clients-partners-logos/client-logo-white-philamuseum.png" />
+
+							
+						</a>
+					</div>
+					
+					<div class="col-sm-4">
+						
+						<a href="#" class="feat-content-block" style="background: linear-gradient(rgba(45,49,66, 0.75), rgba(45,49,66, 0.75)), url('<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg');">
+							
+							<h5>Interactive Exhibits</h5>
+							
+							<h3>Zhizua Temple Ceiling Interactive</h3>
+							
+							<img src="<?php printThemePath(); ?>/assets/clients-partners-logos/client-logo-white-philamuseum.png" />
+
+							
+						</a>
+					</div>
+					
+					<div class="col-sm-4">
+						
+						<a href="#" class="feat-content-block" style="background: linear-gradient(rgba(45,49,66, 0.75), rgba(45,49,66, 0.75)), url('<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg');">
+							
+							<h5>Interactive Exhibits</h5>
+							
+							<h3>Zhizua Temple Ceiling Interactive</h3>
+							
+							<img src="<?php printThemePath(); ?>/assets/clients-partners-logos/client-logo-white-philamuseum.png" />
+
+							
+						</a>
+					</div>
+					
+					<div class="col-sm-4">
+						
+						<a href="#" class="feat-content-block" style="background: linear-gradient(rgba(45,49,66, 0.75), rgba(45,49,66, 0.75)), url('<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg');">
+							
+							<h5>Interactive Exhibits</h5>
+							
+							<h3>Zhizua Temple Ceiling Interactive</h3>
+							
+							<img src="<?php printThemePath(); ?>/assets/clients-partners-logos/client-logo-white-philamuseum.png" />
+
+							
+						</a>
+					</div>
+					
+					<div class="col-sm-4">
+						
+						<a href="#" class="feat-content-block" style="background: linear-gradient(rgba(45,49,66, 0.75), rgba(45,49,66, 0.75)), url('<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg');">
+							
+							<h5>Interactive Exhibits</h5>
+							
+							<h3>Zhizua Temple Ceiling Interactive</h3>
+							
+							<img src="<?php printThemePath(); ?>/assets/clients-partners-logos/client-logo-white-philamuseum.png" />
+
+							
+						</a>
+					</div>
+					
+					<div class="col-sm-4">
+						
+						<a href="#" class="feat-content-block" style="background: linear-gradient(rgba(45,49,66, 0.75), rgba(45,49,66, 0.75)), url('<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg');">
+							
+							<h5>Interactive Exhibits</h5>
+							
+							<h3>Zhizua Temple Ceiling Interactive</h3>
+							
+							<img src="<?php printThemePath(); ?>/assets/clients-partners-logos/client-logo-white-philamuseum.png" />
+
+							
+						</a>
+					</div>
+					
+					<div class="col-sm-4">
+						
+						<a href="#" class="feat-content-block" style="background: linear-gradient(rgba(45,49,66, 0.75), rgba(45,49,66, 0.75)), url('<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg');">
+							
+							<h5>Interactive Exhibits</h5>
+							
+							<h3>Zhizua Temple Ceiling Interactive</h3>
+							
+							<img src="<?php printThemePath(); ?>/assets/clients-partners-logos/client-logo-white-philamuseum.png" />
+
+							
+						</a>
+					</div>
+					
+					<div class="col-sm-4">
+						
+						<a href="#" class="feat-content-block" style="background: linear-gradient(rgba(45,49,66, 0.75), rgba(45,49,66, 0.75)), url('<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg');">
+							
+							<h5>Interactive Exhibits</h5>
+							
+							<h3>Zhizua Temple Ceiling Interactive</h3>
+							
+							<img src="<?php printThemePath(); ?>/assets/clients-partners-logos/client-logo-white-philamuseum.png" />
+
+							
+						</a>
+					</div>
+					
+					
+				</div>
+			</section>
+	</article>
+
+	
+</main>
+
+<?php get_footer(); ?>
