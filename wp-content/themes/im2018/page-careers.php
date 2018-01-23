0e36af42e5416ee403bce97e32c9c9aa1cc8b1d6
Christina Deemer <cahdeemer@gmail.com>
2018-01-23 15:59:51 -0500

    careers page

--- wp-content/themes/im2018/page-careers.php
+++ wp-content/themes/im2018/page-careers.php
@@ -8,7 +8,201 @@
 get_header(); ?>
 
 <main>
-	<h1>Careers Template</h1>
+	<div class="jumbotron">
+			<div class="media-container">
+				<img src="<?php printThemePath(); ?>/assets/backgrounds/background-01@1x.jpg" />
+			</div>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-sm-8">
+						<h1>Come help us make an impact</h1>
+					</div>
+					<div class="col-sm-9">
+						<p>We're a small, close-knit group of designers, developers, and content specialists who build amazing things, and we'd love for you to join us.</p>
+						
+						<a class="btn btn-default" href="#" role="button">View Open Positions</a>
+					</div>
+					
+					
+				</div>
+  			</div>
+	</div>
+	
+	<article class="careers-main">
+		<section class="container-fluid">
+			<div class="row">
+				<div class="col-sm-8 col-sm-offset-2 content-wrapper">
+					<p class="lead">Interactive Mechanics has two priorities: <a href="">our team</a> and our clients.  Our team is actively involved in the growth and betterment of the company through regular start-stop-continue meetings, an annual planning retreat, and monthly one-on-ones with the owner.  Our employee benefits include:</p>
+					
+					
+					<div class="values-wrapper">
+					
+						<div class="col-sm-2">
+						
+							<img src="<?php printThemePath(); ?>/assets/value-icons/value-icon-01@1x.png" />
+						
+						</div>
+						
+						<div class="col-sm-10">
+							
+							<h5>Unlimited Paid Time Off</h5>
+							<p>We belief in healthy work-life balance, so we suggest that employees take a minimum of two weeks off to relax and recharge. In addition to seven paid holidays, we also take off the entire week between Christmas and New Years and have flexible work hours.</p>
+						
+						</div>
+				
+					</div>
+					
+					<div class="values-wrapper">
+					
+						<div class="col-sm-2">
+						
+							<img src="<?php printThemePath(); ?>/assets/value-icons/value-icon-01@1x.png" />
+						
+						</div>
+						
+						<div class="col-sm-10">
+							
+							<h5>Unlimited Paid Time Off</h5>
+							<p>We belief in healthy work-life balance, so we suggest that employees take a minimum of two weeks off to relax and recharge. In addition to seven paid holidays, we also take off the entire week between Christmas and New Years and have flexible work hours.</p>
+
+						
+						</div>
+				
+					</div>
+					
+					<div class="values-wrapper">
+					
+						<div class="col-sm-2">
+						
+							<img src="<?php printThemePath(); ?>/assets/value-icons/value-icon-01@1x.png" />
+						
+						</div>
+						
+						<div class="col-sm-10">
+							
+							<h5>Unlimited Paid Time Off</h5>
+							<p>We belief in healthy work-life balance, so we suggest that employees take a minimum of two weeks off to relax and recharge. In addition to seven paid holidays, we also take off the entire week between Christmas and New Years and have flexible work hours.</p>
+
+						
+						</div>
+				
+					</div>
+					
+					<div class="values-wrapper">
+					
+						<div class="col-sm-2">
+						
+							<img src="<?php printThemePath(); ?>/assets/value-icons/value-icon-01@1x.png" />
+						
+						</div>
+						
+						<div class="col-sm-10">
+							
+							<h5>Unlimited Paid Time Off</h5>
+							<p>We belief in healthy work-life balance, so we suggest that employees take a minimum of two weeks off to relax and recharge. In addition to seven paid holidays, we also take off the entire week between Christmas and New Years and have flexible work hours.</p>
+						
+						</div>
+				
+					</div>
+					
+					<div class="values-wrapper">
+					
+						<div class="col-sm-2">
+						
+							<img src="<?php printThemePath(); ?>/assets/value-icons/value-icon-01@1x.png" />
+						
+						</div>
+						
+						<div class="col-sm-10">
+							
+							<h5>Unlimited Paid Time Off</h5>
+							<p>We belief in healthy work-life balance, so we suggest that employees take a minimum of two weeks off to relax and recharge. In addition to seven paid holidays, we also take off the entire week between Christmas and New Years and have flexible work hours.</p>
+
+						
+						</div>
+				
+					</div>
+					
+					
+					<div class="values-wrapper">
+					
+						<div class="col-sm-2">
+						
+							<img src="<?php printThemePath(); ?>/assets/value-icons/value-icon-01@1x.png" />
+						
+						</div>
+						
+						<div class="col-sm-10">
+							
+							<h5>Unlimited Paid Time Off</h5>
+							<p>We belief in healthy work-life balance, so we suggest that employees take a minimum of two weeks off to relax and recharge. In addition to seven paid holidays, we also take off the entire week between Christmas and New Years and have flexible work hours.</p>
+
+							
+						</div>
+				
+					</div>
+										
+				
+				</div>
+			</div>
+		
+		</section>
+	</article>
+	
+	<article class="careers-main">
+		<section class="container-fluid">
+			<div class="row">
+				<div class="col-sm-8 col-sm-offset-2 content-wrapper">
+					
+					<h5>About the Project</h5>
+					
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula dui sed risus bibendum tempor in eget velit. Integer sit amet porttitor nibh. Duis dictum justo a neque facilisis, a ullamcorper augue fermentum. Etiam consectetur magna ut metus lobortis malesuada. Nulla varius suscipit nisi non tincidunt. Etiam vitae lobortis tellus. Fusce tellus massa, aliquam at finibus vel, bibendum ac nunc. Nullam ornare, sem quis scelerisque dapibus, diam erat dignissim urna, at efficitur nunc nisi vitae dolor. Proin sit amet dolor mi. Maecenas commodo nulla tellus, vel vestibulum dui vestibulum ut. Sed id fringilla urna. In eros eros, mattis in mattis quis, molestie ac sapien. Curabitur maximus leo a placerat lobortis. Nullam venenatis, neque eget mollis elementum, neque purus dapibus quam, sed facilisis neque leo id justo.</p>
+					
+										
+					<h5>Kid Friendly &amp; Accessible</h5>
+					
+					<p>Nullam dictum faucibus semper. Curabitur ut leo non urna egestas malesuada vel sed eros. Nam augue enim, pretium a posuere vel, auctor in ipsum. Nulla ultricies placerat velit, non sagittis metus tincidunt vitae. Donec id sapien sollicitudin, blandit nisi a, elementum diam. Mauris commodo gravida purus, vel pretium risus. Nullam rhoncus elit maximus, blandit risus pretium, rhoncus turpis. Quisque ultricies laoreet eleifend. </p>
+					
+															
+				</div><!-- /.content-wrapper -->
+			</div><!-- /.row -->
+		</section>
+	</article>
+	
+	<article id="project-details">
+		<section class="container-fluid">
+			<div class="row">
+				
+				<div class="col-sm-8 col-sm-offset-2">
+					
+					<h3>All Open Positions</h3>
+					
+					<div class="col-sm-6 learn-more-wrapper">
+						<div>
+						<h4>Interactive Designer</h4>
+						<h5>Fulltime Position in Philadelphia</h5>
+						
+						<button class="btn btn-default">Apply Now!</button>
+						</div>
+							
+					</div>
+					
+					<div class="col-sm-6 learn-more-wrapper">
+						<div>
+							<h4>Interactive Developer</h4>
+							<h5>Fulltime Position in Philadelphia</h5>
+							<button class="btn btn-default">Apply Now!</button>
+						</div>	
+						
+					</div>
+					
+				</div>
+				
+			</div>
+		</section>
+	</article>
+
+
 	
 	
 </main>
