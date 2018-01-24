93a9d219c660ffdd81351e645b92833d29d4f767
Christina Deemer <cahdeemer@gmail.com>
2018-01-24 13:48:33 -0500

    webinars archive

---
+++ wp-content/themes/im2018/archive-webinars.php
@@ -0,0 +1,225 @@
+<?php
+/**
+ * Template Name: Webinars Archive
+ *
+ * @package im2018
+ * @since im2018 1.0
+ */
+get_header(); ?>
+
+
+<main>
+	<div class="jumbotron jumbo-short">
+			<div class="media-container">
+				<img src="<?php printThemePath(); ?>/assets/backgrounds/background-01@1x.jpg" />
+			</div>
+			
+			<div class="container-fluid">
+				<div class="row">
+					
+					<div class="col-sm-7">
+						<h1>Webinars</h1>
+					</div>
+					
+					<div class="col-sm-7">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent maximus mi et orci efficitur pulvinar.</p>
+			
+					</div>
+					
+				</div>	
+  			</div>
+	</div>
+	
+	<article id="cs-archive">
+		<section class="container-fluid">
+				<div class="row">
+										
+					
+								
+					<div class="col-sm-4">
+						
+						<a href="#" class="feat-content-block" style="background: linear-gradient(rgba(71,142,187, 0.7), rgba(58,89,141, 0.7)), url('<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg');">
+							
+							<h5><span>2:00 PM</span> / <span>May 22, 2017</span></h5>
+							
+							<h3>Lorem ipsum dolor sit amet</h3>
+							
+						</a>
+					</div>
+					
+					<div class="col-sm-4">
+						
+						<a href="#" class="feat-content-block" style="background: linear-gradient(rgba(71,142,187, 0.7), rgba(58,89,141, 0.7)), url('<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg');">
+							
+							<h5><span>2:00 PM</span> / <span>May 22, 2017</span></h5>
+							
+							<h3>Lorem ipsum dolor sit amet</h3>
+							
+						</a>
+					</div>					
+					
+					
+				</div>
+			</section>
+	</article>
+	
+	<article class="webinars-main">
+		<section class="container-fluid">
+			<div class="row">
+				<div class="col-sm-8 col-sm-offset-2 content-wrapper">
+					
+					<div class="col-sm-12">
+						<h3>Past Webinars</h3>
+					</div>
+										
+					
+					<div class="webinars-wrapper col-sm-12">
+					
+						<div class="col-sm-2">
+						
+							<img src="<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg" />
+						
+						</div>
+						
+						<div class="col-sm-10">
+							
+							<h4>Unlimited Paid Time Off</h4>
+							<p>We belief in healthy work-life balance, so we suggest that employees take a minimum of two weeks off to relax and recharge. In addition to seven paid holidays, we also take off the entire week between Christmas and New Years and have flexible work hours.</p><a href="" class="read-more">Learn More</a>
+						
+						</div>
+				
+					</div>
+					
+					<div class="webinars-wrapper col-sm-12">
+					
+						<div class="col-sm-2">
+						
+							<img src="<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg" />
+						
+						</div>
+						
+						<div class="col-sm-10">
+							
+							<h4>Unlimited Paid Time Off</h4>
+							<p>We belief in healthy work-life balance, so we suggest that employees take a minimum of two weeks off to relax and recharge. In addition to seven paid holidays, we also take off the entire week between Christmas and New Years and have flexible work hours.</p><a href="" class="read-more">Learn More</a>
+
+						
+						</div>
+				
+					</div>
+					
+					<div class="webinars-wrapper col-sm-12">
+					
+						<div class="col-sm-2">
+						
+							<img src="<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg" />
+						
+						</div>
+						
+						<div class="col-sm-10">
+							
+							<h4>Unlimited Paid Time Off</h4>
+							<p>We belief in healthy work-life balance, so we suggest that employees take a minimum of two weeks off to relax and recharge. In addition to seven paid holidays, we also take off the entire week between Christmas and New Years and have flexible work hours.</p><a href="" class="read-more">Learn More</a>
+
+						
+						</div>
+				
+					</div>
+					
+					<div class="webinars-wrapper col-sm-12">
+					
+						<div class="col-sm-2">
+						
+							<img src="<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg" />
+						
+						</div>
+						
+						<div class="col-sm-10">
+							
+							<h4>Unlimited Paid Time Off</h4>
+							<p>We belief in healthy work-life balance, so we suggest that employees take a minimum of two weeks off to relax and recharge. In addition to seven paid holidays, we also take off the entire week between Christmas and New Years and have flexible work hours.</p><a href="" class="read-more">Learn More</a>
+						
+						</div>
+				
+					</div>
+					
+					<div class="webinars-wrapper col-sm-12">
+					
+						<div class="col-sm-2">
+						
+							<img src="<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg" />
+						
+						</div>
+						
+						<div class="col-sm-10">
+							
+							<h4>Unlimited Paid Time Off</h4>
+							<p>We belief in healthy work-life balance, so we suggest that employees take a minimum of two weeks off to relax and recharge. In addition to seven paid holidays, we also take off the entire week between Christmas and New Years and have flexible work hours.</p><a href="" class="read-more">Learn More</a>
+
+						
+						</div>
+				
+					</div>
+					
+					
+					<div class="webinars-wrapper col-sm-12">
+					
+						<div class="col-sm-2">
+						
+							<img src="<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg" />
+						
+						</div>
+						
+						<div class="col-sm-10">
+							
+							<h4>Unlimited Paid Time Off</h4>
+							<p>We belief in healthy work-life balance, so we suggest that employees take a minimum of two weeks off to relax and recharge. In addition to seven paid holidays, we also take off the entire week between Christmas and New Years and have flexible work hours.</p><a href="" class="read-more">Learn More</a>
+
+							
+						</div>
+				
+					</div>
+										
+				
+				</div>
+			</div>
+		
+		</section>
+	</article>
+	
+	<article id="project-details">
+		<section class="container-fluid">
+			<div class="row">
+				
+				<div class="col-sm-8 col-sm-offset-2">
+					
+					<h3>Have a webinar topic or idea?</h3>
+					
+					
+					<div class="learn-more-wrapper">
+						<img class="staff-thumb-sm"src="<?php printThemePath(); ?>/assets/staff-thumbnails/staff-thumbnail-longo@1x.jpg" />
+						
+					<div>
+						
+						<h4>LaNeshe Miller-White</h4>
+						<h5>Marketing Manager</h5>
+						<h4><a href="mailto:someone@example.com">laneshe@interactivemechanics.com</a></h4>
+					</div>
+												
+				</div>
+				
+			</div>
+		</section>
+	</article>
+
+
+	
+	
+
+
+	
+
+</main>
+
+
+<?php get_footer(); ?>
