b96beaee590889443f0cb3aa94babcc9d36abe29
Christina Deemer <cahdeemer@gmail.com>
2018-01-24 15:12:52 -0500

    staff single

--- wp-content/themes/im2018/single-team_bios.php
+++ wp-content/themes/im2018/single-team_bios.php
@@ -12,7 +12,123 @@
 <?php while ( have_posts() ) : the_post();
 ?>
 
-<h1>This is single team bio</h1>
+<div class="jumbotron jumbo-short">
+	<div class="media-container">
+		<img src="<?php printThemePath(); ?>/assets/backgrounds/background-01@1x.jpg" />
+	</div>				
+</div>
+
+
+<article>
+		<section class="container-fluid">
+				<div class="row">
+					
+					<div class="col-sm-12">
+						<div class="feat-content-block-wide"  style="background-image: linear-gradient(45deg, rgba(71,142,187, 1.0), rgba(58,89,141, 0.1));">
+							
+							<h5>Director of Strategic Initiatives</h5>
+							
+							<h3>Amelia Longo</h3>
+							
+							
+							<div id="bio-social" class="social list-group">
+								<a href="#" target="_blank" class="list-group-item"><?php include("assets/icons/icon-twitter.svg"); ?></a>
+								<a href="#" target="_blank" class="list-group-item"><?php include("assets/icons/icon-linkedin.svg"); ?></a>
+								<a href="#" target="_blank" class="list-group-item"><?php include("assets/icons/icon-email.svg"); ?></a>
+							</div>
+														
+						</div>
+					</div>
+				</div>
+		</section>
+</article>
+
+<article class="entry-content">
+		<section class="container-fluid">
+			<div class="row">
+				<div class="col-sm-8 col-sm-offset-2 content-wrapper">
+					<h5>Biography</h5>
+					
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vehicula dui sed risus bibendum tempor in eget velit. Integer sit amet porttitor nibh. Duis dictum justo a neque facilisis, a ullamcorper augue fermentum. Etiam consectetur magna ut metus lobortis malesuada. Nulla varius suscipit nisi non tincidunt. Etiam vitae lobortis tellus. Fusce tellus massa, aliquam at finibus vel, bibendum ac nunc. Nullam ornare, sem quis scelerisque dapibus, diam erat dignissim urna, at efficitur nunc nisi vitae dolor. Proin sit amet dolor mi. Maecenas commodo nulla tellus, vel vestibulum dui vestibulum ut. Sed id fringilla urna. In eros eros, mattis in mattis quis, molestie ac sapien. Curabitur maximus leo a placerat lobortis. Nullam venenatis, neque eget mollis elementum, neque purus dapibus quam, sed facilisis neque leo id justo.</p>
+					
+					
+				</div><!-- /.content-wrapper -->
+			</div><!-- /.row -->
+		</section>
+	</article>
+	
+
+	<article id="related-content" class="team bottom-border">
+		<section class="container-fluid">
+			<div class="row">
+				
+					<div class="col-sm-12 related-content-heading">
+						<h3>Our Team</h3>
+					</div>
+				
+			
+					<div class="col-sm-4">
+						
+						<a href="#" class="feat-content-block" style="background: linear-gradient(45deg, rgba(71,142,187, 1.0), rgba(58,89,141, 0.1)), url('<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg');">
+							
+							<h5>Director of Strategic Initiatives</h5>
+							
+							<h3>Amelia Longo</h3>
+							
+						</a>
+					</div>
+					
+					<div class="col-sm-4">
+						
+						<a href="#" class="feat-content-block" style="background: linear-gradient(45deg, rgba(71,142,187, 1.0), rgba(58,89,141, 0.1)), url('<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg');">
+							
+							<h5>Chief Communications Officer</h5>
+							
+							<h3>Nyota Uhura</h3>
+							
+						</a>
+					</div>
+					
+					<div class="col-sm-4">
+						
+						<a href="#" class="feat-content-block" style="background: linear-gradient(45deg, rgba(71,142,187, 1.0), rgba(58,89,141, 0.1)), url('<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg');">
+							
+							<h5>Interactive Designer</h5>
+							
+							<h3>Tara Espenshade</h3>
+							
+						</a>
+					</div>
+					
+					<div class="col-sm-4">
+						
+						<a href="#" class="feat-content-block" style="background: linear-gradient(45deg, rgba(71,142,187, 1.0), rgba(58,89,141, 0.1)), url('<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg');">
+							
+							<h5>Marketing Manager</h5>
+							
+							<h3>LaNeshe Miller-White</h3>
+							
+						</a>
+					</div>
+					
+					<div class="col-sm-4">
+						
+						<a href="#" class="feat-content-block" style="background: linear-gradient(45deg, rgba(71,142,187, 1.0), rgba(58,89,141, 0.1)), url('<?php printThemePath(); ?>/assets/thumbnail-images/thumbnail-image-01@1x.jpg');">
+							
+							<h5>Defense Against The Dark Arts</h5>
+							
+							<h3>Severus Snape</h3>
+							
+						</a>
+					</div>		
+									
+			</div>
+		</section>
+	</article>
+
+
+
+
 
 <?php endwhile; ?>
 
